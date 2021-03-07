f7d59cdc59edb2890c6994f07705701991db3595
MrMelon <github@onpointcoding.net>
2021-03-07 23:25:43 +0000

    New credits screen

--- commands/credits.js
+++ commands/credits.js
@@ -1,18 +1,12 @@
-const fs = require("fs");
 const Discord = require("discord.js");
 
 function creditsCommand(bot, msg, args = []) {
-  if (fs.existsSync(bot.creditsPath)) {
-    fs.readFile(bot.creditsPath, (err, data) => {
-      if (err) throw new Error("Error: Unable to find credits");
-      var embed = new Discord.MessageEmbed()
-        .setColor("#15d0ed")
-        .setAuthor("Minesweeper!", bot.jsonfile.logoQuestion)
-        .setTitle("Credits")
-        .setDescription(data.toString());
-      msg.channel.send(embed);
-    });
-  }
+  var embed = new Discord.MessageEmbed()
+    .setColor("#15d0ed")
+    .setAuthor("Minesweeper!", bot.jsonfile.logoQuestion)
+    .setTitle("Credits")
+    .addFields(bot.CREDITS);
+  msg.channel.send(embed);
 }
 
 var helpExample = [
